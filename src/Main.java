1077fe76e7f2842b604c4d6493f73a8cd98cbd3b
ArturLLopes <arturllopes@gmail.com>
2026-01-22 16:03:57 -0300

    feat: finalizando o desafio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -25,9 +23,37 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devArtur = new Dev();
+        devArtur.setNome("Artur");
+        devArtur.inscreverBootCamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devArtur .getConteudosInscritos());
+        System.out.println("Conteudos concluidos" + devArtur .getConteudosConcluidos());
+        devArtur.progredir();
+        devArtur.progredir();
+        System.out.println("Conteudos concluidos" + devArtur .getConteudosConcluidos());
+        System.out.println("XP: " + devArtur.calcularToralXp());
+
+        Dev devDiego = new Dev();
+        devDiego.setNome("Diego");
+        devDiego.inscreverBootCamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devDiego .getConteudosInscritos());
+        System.out.println("Conteudos concluidos" + devDiego .getConteudosConcluidos());
+        devDiego.progredir();
+        System.out.println("Conteudos concluidos" + devDiego .getConteudosConcluidos());
+        System.out.println("XP: " + devDiego.calcularToralXp());
+
 
     }
 }
